426963e5d4fb6b5ec0451c5cefab2079e03c729c
roxzlir <emil.ch.nordin@gmail.com>
2024-08-23 12:46:27 +0200

    Added function so that the bot now reply in same language that you ask the question with!

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Azure.AI.Translation.Text;
 using Azure;
 using Microsoft.Extensions.Configuration;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MyQ_ABot
 {
@@ -38,8 +39,8 @@ namespace MyQ_ABot
             QuestionAnsweringClient client = new QuestionAnsweringClient(endpoint, credential);
             QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
 
-            Console.WriteLine("Now chatting with RoxBot!");
-            Console.WriteLine("(to quit, just ask quit)");
+            Console.WriteLine("Now chatting with RoxBot! (to quit, just ask me to quit)");
+            
 
             while (true)
             {
@@ -47,29 +48,44 @@ namespace MyQ_ABot
                 Console.Write("Me: ");
                 // Tar emot en fråga på valfritt språk
                 string question = Console.ReadLine();
+
+                //Här vill jag ta reda på vilket språk som man har skrivit frågan på
+                Response<IReadOnlyList<TranslatedTextItem>> resp = await translatorClient.TranslateAsync("en", question).ConfigureAwait(false);
+                IReadOnlyList<TranslatedTextItem> translations = resp.Value;
+                TranslatedTextItem translation = translations.FirstOrDefault();
+                // Och sparar ner språk koden till language
+                string language = translation?.DetectedLanguage.Language;
+
                 // Översätter frågan till engelska med metoden TranslateTextAsync
-                string translatedQuestion = await TranslateTextAsync(translatorClient, question);
+                string translatedQuestion = await TranslateTextAsync(translatorClient, question, "en");
                 //Tar emot ett svar en det kommer som ett paket
                 Response<AnswersResult> response = client.GetAnswers(translatedQuestion, project);
+
                 // Så måste loopa då det ligger som en generic lista,då vill jag få ut VÄRDET och ANSWERS
+                var botAnswer = "";
                 foreach (KnowledgeBaseAnswer answer in response.Value.Answers)
                 {
-                    Console.WriteLine($"RoxBot: {answer.Answer}");
+                    // Och här där jag kommer åt botens svar, så översätter jag det till samma språk som det man ställde fråga på genom att skicka med den språk koden
+                    botAnswer = await TranslateTextAsync(translatorClient, answer.Answer, language);
+                    Console.WriteLine($"RoxBot: {botAnswer}");
                 }
+
 
                 if (translatedQuestion.ToLower() == "quit") break;
             }
         }
 
         // Gör en metod som tar emot en tranlator klient samt den text jag vill få översatt till engelska åt min bot
-        static async Task<string> TranslateTextAsync(TextTranslationClient translatorClient, string text)
+        static async Task<string> TranslateTextAsync(TextTranslationClient translatorClient, string text, string targetLang)
         {
-            string targetLanguage = "en";
+            string targetLanguage = targetLang;
             var response = await translatorClient.TranslateAsync(targetLanguage, new List<string> { text }).ConfigureAwait(false);
 
             // Returnera den översatta texten
             return response.Value[0].Translations[0].Text;
         }
+ 
+
     }
 }
 
